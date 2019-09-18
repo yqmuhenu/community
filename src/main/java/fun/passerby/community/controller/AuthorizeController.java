60952b4e70a8ae7346eb33c85ba14a4332bcfcc8
yqmuhenu <muyaqi.henu@foxmail.com>
2019-09-18 21:13:38 +0800

    realize add gitHubUser to tb_user

--- src/main/java/fun/passerby/community/controller/AuthorizeController.java
+++ src/main/java/fun/passerby/community/controller/AuthorizeController.java
@@ -1,13 +1,18 @@
 package fun.passerby.community.controller;
 
+import fun.passerby.community.domain.User;
 import fun.passerby.community.dto.AccessTokenDTO;
 import fun.passerby.community.dto.GithubUser;
-import fun.passerby.community.provider.GithubProvider;
+import fun.passerby.community.service.UserService;
+import fun.passerby.community.util.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -22,9 +27,13 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
-                           @RequestParam(name = "state")String state){
+                           @RequestParam(name = "state")String state,
+                           HttpServletRequest request){
         //构造accessTokenDTO对象
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
@@ -35,8 +44,25 @@ public class AuthorizeController {
         //根据accessTokenDTO获取access_token
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         //根据access_token获取user信息
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user);
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        //System.out.println(user);
+        if (githubUser != null){
+            User user = new User();
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            user.setToken(UUID.randomUUID().toString());
+            //向数据库中插入
+            userService.addUser(user);
+            //登录成功
+            request.getSession().setAttribute("user",githubUser);
+            request.getSession().setMaxInactiveInterval(1800);
+            //System.out.println(githubUser);
+            return "redirect:/";
+        }else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
